7c1e0dc7a7f863de50b30341ad14752b1ed9833e
TheBunnyBad <santi-c-b@outlook.com>
2019-03-05 11:41:01 -0500

    Migrated

---
+++ src/Components/about.js
@@ -0,0 +1,5 @@
+function About() {
+    return <div>Welcome to About xd</div>
+  }
+  
+  export default About;
---
+++ src/helpers/api.js
@@ -0,0 +1,18 @@
+import Houses from '../data/houses.js';
+import Admins from '../data/houses.js';
+import RT from '../data/rt.js';
+import Crypto from './crypto.js';
+
+getHouses = () => {
+    //TODO: Call API and get houses
+    return Houses;
+}
+
+findAdmin = (admin) => {
+    console.log(CryptoJS.AES.encrypt(admin.password))
+    Admins.find(admin)
+}
+
+export default {
+    findAdmin: (a) => { console.log(a)}
+}
