8e640fce5c54637646f175195951ffd5b8a808c7
TheBunnyBad <santi-c-b@outlook.com>
2019-03-06 23:22:35 -0500

    Updates

---
+++ precache-manifest.029401f62be83b67f22ca7c6ca523814.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a6908b4352b5a2c2a8a",
+    "url": "/Airpal/static/js/runtime~main.7a6908b4.js"
+  },
+  {
+    "revision": "a1d60490db95d81923d3",
+    "url": "/Airpal/static/js/main.a1d60490.chunk.js"
+  },
+  {
+    "revision": "7ba2f32253a913fb6f1a",
+    "url": "/Airpal/static/js/2.7ba2f322.chunk.js"
+  },
+  {
+    "revision": "a1d60490db95d81923d3",
+    "url": "/Airpal/static/css/main.0b130d81.chunk.css"
+  },
+  {
+    "revision": "4110db2708e1a03457ea213a35061435",
+    "url": "/Airpal/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Airpal/precache-manifest.029401f62be83b67f22ca7c6ca523814.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Airpal/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
