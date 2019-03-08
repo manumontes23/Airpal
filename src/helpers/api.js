cc5a932907752707037b6019b9a270394a5e6433
TheBunnyBad <santi-c-b@outlook.com>
2019-03-08 12:13:16 -0500

    Me

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -3,8 +3,14 @@ import Houses from '../data/houses.js';
 import RT from '../data/rt.js';
 //import Crypto from './crypto.js';
 
+const urls = {
+    houses: "http://35.237.240.199:3002/api/house"
+}
+
 const getHouses = () => {
     //TODO: Call API and get houses
+    const Houses = fetch()
+    console.log(Houses);
     return Houses;
 }
 
