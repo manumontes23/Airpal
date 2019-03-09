10a73efa1afaa727b2cc1cd2311d11a16c8c85c1
JQartas <jcuartas0611@gmail.com>
2019-03-09 12:29:07 -0500

    Retoques

--- src/Components/FormHouse.js
+++ src/Components/FormHouse.js
@@ -29,7 +29,7 @@ const styles = theme => ({
     margin: theme.spacing.unit,
     width: theme.spacing.unit * 15,
     backgroundColor: '#e0e0e0',
-    borderRadius: 5
+    borderRadius: 5,
   },
   heading: {
     color: '#fff'
@@ -40,7 +40,8 @@ const styles = theme => ({
   },
   dense: {
     marginTop: 20,
-    backgroundColor: '#424242'
+    backgroundColor: '#424242',
+    borderRadius: 5
   },
   details: {
     display: 'block',
@@ -48,9 +49,12 @@ const styles = theme => ({
   },
   button: {
     borderRadius: 5,
-    marginTop: 10,
+    marginTop: 15,
     marginBottom: 10,
-    padding: 10
+  },
+  helperText: {
+    color: '#424242',
+    fontStyle: 'italic'
   }
 });
 
@@ -72,7 +76,20 @@ class FormHouse extends React.Component {
   state = {
     revoked: false,
   }
+
+  /**
+   * Envia la petición HTTP con la información del formulario
+   */
+  sendForm() {
+    
+  }
   
+  /**
+   * Cambia el estade segun el evento que se ejecute
+   * Param: 
+   * name:String que indica el nombre del atributo 
+   * del estado que se desea cambiar
+   */
   handleChange = name => event => {
     this.setState({
       [name]: event.target.value,
@@ -91,54 +108,54 @@ class FormHouse extends React.Component {
               <Typography className={classes.heading} variant='title'>Información de habitante</Typography>
             </ExpansionPanelSummary>
             <ExpansionPanelDetails className={classes.details}>
-            <TextField 
-                id='house-name'
-                name='NAME'
-                variant='filled'
-                label='Nombre'
-                required={true}
-                className={classes.textField}
-              />
               <TextField 
-              id='house-lastname'
-              name='LASTNAME'
-              variant='filled'
-              required={true}
-              label='Apellidos'
-              required={true}
-              className={classes.textField}/>
-              <TextField 
-              id='house-email'
-              name='EMAIL'
-              label='Email'
-              variant='filled'
-              type='EMAIL'
-              required={true}
-              className={classes.textField}/>
-              <TextField 
-              id='house-telnumber'
-              name='TELNUMBER'
-              label='Telefono'
-              required={true}
-              variant='filled'
-              className={classes.textField}
-              type='number'
-              />
-              <TextField 
-              id='house-address'
-              name='ADDRESS'
-              variant='filled'
-              label='Dirección'
-              required={true}
-              className={classes.textField}/>
-              <TextField 
-              id='houses-resident-number'
-              name='RESIDENTNUMBER'
-              variant='filled'
-              label='Número de residentes'
-              required={true}
-              type='number'
-              className={classes.textField}/>
+                  id='house-name'
+                  name='NAME'
+                  variant='filled'
+                  label='Nombre'
+                  required={true}
+                  className={classes.textField}
+                />
+                <TextField 
+                  id='house-lastname'
+                  name='LASTNAME'
+                  variant='filled'
+                  required={true}
+                  label='Apellidos'
+                  required={true}
+                  className={classes.textField}/>
+                <TextField 
+                  id='house-email'
+                  name='EMAIL'
+                  label='Email'
+                  variant='filled'
+                  type='EMAIL'
+                  required={true}
+                  className={classes.textField}/>
+                <TextField 
+                  id='house-telnumber'
+                  name='TELNUMBER'
+                  label='Telefono'
+                  required={true}
+                  variant='filled'
+                  className={classes.textField}
+                  type='number'
+                />
+                <TextField 
+                  id='house-address'
+                  name='ADDRESS'
+                  variant='filled'
+                  label='Dirección'
+                  required={true}
+                  className={classes.textField}/>
+                <TextField 
+                  id='houses-resident-number'
+                  name='RESIDENTNUMBER'
+                  variant='filled'
+                  label='Número de residentes'
+                  required={true}
+                  type='number'
+                  className={classes.textField}/>
             </ExpansionPanelDetails>
           </ExpansionPanel>
           <ExpansionPanel className={classes.dense}>
@@ -147,71 +164,77 @@ class FormHouse extends React.Component {
             </ExpansionPanelSummary>
             <ExpansionPanelDetails className={classes.details}>
               <TextField 
-              id='house-floor-number'
-              name='FLOORNUMBER'
-              required={true}
-              variant='filled'
-              label='Numero de piso'
-              helperText='Se elige el numero piso en cuanto a altitud de un edificio'
-              className={classes.textField}
-              type='number'/>
+                id='house-floor-number'
+                name='FLOORNUMBER'
+                required={true}
+                variant='filled'
+                label='Numero de piso'
+                helperText={
+                  <Typography className={classes.helperText}>Segun la altidud del piso en el edificio</Typography>
+                }
+                className={classes.textField}
+                type='number'/>
               <TextField 
-              id='house-revoked'
-              name='REVOKEHOUSE'
-              variant='filled'
-              label='Casa revocada'
-              fullWidth
-              select
-              className={classes.select}
-              value={this.state.revoked}
-              onChange={this.handleChange('revoked')}>
-              {revokedOptions.map(option => (
-                <option key={option.key} value={option.value}>
-                  {option.label}
-                </option>
+                id='house-revoked'
+                name='REVOKEHOUSE'
+                variant='filled'
+                label='Casa revocada'
+                fullWidth
+                select
+                className={classes.select}
+                value={this.state.revoked}
+                onChange={this.handleChange('revoked')}>
+                {revokedOptions.map(option => (
+                  <option key={option.key} value={option.value}>
+                    {option.label}
+                  </option>
               ))}</TextField>
               <TextField 
-              id='house-smokers'
-              variant='filled'
-              name='SMOKERSNUMBER'
-              label='Numero de fumadores'
-              helperText='Si no hay fumadores ingrese 0'
-              type='number'
-              required={true}
-              className={classes.textField}/>
+                id='house-smokers'
+                variant='filled'
+                name='SMOKERSNUMBER'
+                label='Numero de fumadores'
+                helperText={
+                  <Typography className={classes.helperText}>Si no hay fumadores ingrese 0</Typography>
+                }
+                type='number'
+                required={true}
+                className={classes.textField}/>
               <TextField
-              id='house-pet-number'
-              name='PETSNUMBER'
-              label='Numero de mascotas'
-              variant='filled'
-              helperText='Si no hay, ingrese 0'
-              required={true}
-              className={classes.textField}
-              type='number'/>
+                id='house-pet-number'
+                name='PETSNUMBER'
+                label='Numero de mascotas'
+                variant='filled'
+                helperText={
+                  <Typography className={classes.helperText}>Si no hay, ingrese 0</Typography>
+                }
+                required={true}
+                className={classes.textField}
+                type='number'/>
               <TextField 
-              id='house-floor-material'
-              variant='filled'
-              name='FLOORMATERIAL'
-              label='Material del suelo'
-              className={classes.textField}
-              required={true}/>
+                id='house-floor-material'
+                variant='filled'
+                name='FLOORMATERIAL'
+                label='Material del suelo'
+                className={classes.textField}
+                required={true}/>
               <TextField 
-              id='house-walls-material'
-              name='WALLSMATERIAL'
-              variant='filled'
-              label='Material de las paredes'
-              className={classes.textField}
-              required={true}/>
+                id='house-walls-material'
+                name='WALLSMATERIAL'
+                variant='filled'
+                label='Material de las paredes'
+                className={classes.textField}
+                required={true}/>
               <TextField 
-              id='house-paint'
-              variant='filled'
-              name='PAINTTYPE'
-              label='Tipo de pintura'
-              className={classes.textField}
-              required={true}/>
+                id='house-paint'
+                variant='filled'
+                name='PAINTTYPE'
+                label='Tipo de pintura'
+                className={classes.textField}
+                required={true}/>
             </ExpansionPanelDetails>
           </ExpansionPanel>
-          <Button variant='raised' className={classes.button}>Registrar</Button>
+          <Button variant='raised' className={classes.button} onSubmit={this.sendForm}>Registrar</Button>
         </form>
       </div>
     );
