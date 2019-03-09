b7690e3414c11b928cf9ba1f807f7241e4666184
fxfxrxfx <santi-c-b@outlook.com>
2019-03-09 04:17:59 -0500

    Updates

--- precache-manifest.5e8a767651c615e90faa687956e59ec5.js
+++ precache-manifest.5e8a767651c615e90faa687956e59ec5.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Airpal/static/js/runtime~main.7a6908b4.js"
   },
   {
-    "revision": "4c209b8fc879edb89403",
-    "url": "/Airpal/static/js/main.4c209b8f.chunk.js"
+    "revision": "69c46e66fa10a65b1a78",
+    "url": "/Airpal/static/js/main.69c46e66.chunk.js"
   },
   {
     "revision": "648a65e2f3e412715914",
     "url": "/Airpal/static/js/2.648a65e2.chunk.js"
   },
   {
-    "revision": "4c209b8fc879edb89403",
+    "revision": "69c46e66fa10a65b1a78",
     "url": "/Airpal/static/css/main.3a18453b.chunk.css"
   },
   {
-    "revision": "c662f988e9ca2db27263770f81c6d926",
+    "revision": "ee28d2f5788d0b92c80210970b28d090",
     "url": "/Airpal/index.html"
   }
 ];
