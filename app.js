c4db5e0d9d220eba1e0d25e4e6cb445a9bff7179
fxfxrxfx <santi-c-b@outlook.com>
2019-05-03 01:09:55 -0500

    Cors

--- app.js
+++ app.js
@@ -6,27 +6,15 @@ const app = express();
 const apiRouter = require('./routes/apiRoutes');
 
 
-const cors = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-}
 
-app.use(cors);
+const cors = require('cors');
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const config = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-}
-
-app.use(config);
 
 //Main route for the api backend of the server
 app.use('/api', apiRouter);
