a79fdaeeea1761fb48bb8043f9c06a5e586d0909
fxfxrxfx <30848622+fxfxrxfx@users.noreply.github.com>
2019-05-07 12:47:38 -0500

    Update dbconf.js

--- config/dbconf.js
+++ config/dbconf.js
@@ -2,8 +2,8 @@ const args = require('./input');
 
 const data = {
   host: 'remotemysql.com',
-  user: args.user,
-  password: args.password, //Medell1n
+  user: "ae1Q6As4pi",   //args.user,
+  password: "ppAjm33oL1",//args.password, //Medell1n
   database: 'ae1Q6As4pi'
 } 
 console.log(data);
